c96ebd5efc72a8c5194ead23ce0ca03389d1f316
why2001x <dnd2001@qq.com>
2020-06-30 21:35:18 +0800

    Comparators reset

--- Comparators.vhd
+++ Comparators.vhd
@@ -5,25 +5,22 @@ PACKAGE Comparators IS
 
 COMPONENT comparator4 IS
 	PORT (
-		aI, bI: IN std_logic_vector(3 downto 0);
-		albI, agbI, aebI: IN std_logic;
-		albO, agbO, aebO: OUT std_logic
+		dataa, datab: IN std_logic_vector(3 downto 0);
+		aeb, agb, alb: OUT std_logic
 	);
 END COMPONENT;
 
 COMPONENT comparator8 IS
 	PORT (
-		aI, bI: IN std_logic_vector(7 downto 0);
-		albI, agbI, aebI: IN std_logic;
-		albO, agbO, aebO: OUT std_logic
+		dataa, datab: IN std_logic_vector(7 downto 0);
+		aeb, agb, alb: OUT std_logic
 	);
 END COMPONENT;
 
 COMPONENT comparatorC IS
 	PORT (
-		aI, bI: IN std_logic_vector(11 downto 0);
-		albI, agbI, aebI: IN std_logic;
-		albO, agbO, aebO: OUT std_logic
+		dataa, datab: IN std_logic_vector(11 downto 0);
+		aeb, agb, alb: OUT std_logic
 	);
 END COMPONENT;
 
