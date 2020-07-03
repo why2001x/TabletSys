f8c064474135508c0cacd108dfb2b80196f43938
n0tva1id <172410245@qq.com>
2020-07-03 20:45:56 +0800

    Bottle Limit Set tested, unexpected redLED in first period

--- TabletSys.vhd
+++ TabletSys.vhd
@@ -14,12 +14,12 @@ USE work.Counters.all;
 --greenLED:正常装瓶时（startpulse启动后）亮--
 --redLED：设定每瓶片数十位时，超过4时报警，固定在4不可增加（最大每瓶片数为49）/halt状态亮/装瓶结束亮
 --numO1-O5：
---待机模式:没定
---设定每瓶十位模式：numO1显示十位
---设定每瓶个位模式：numO1显示十位，numO2显示个位
---准备就绪：全0
+--状态00（初始状态）：numO1显示设定瓶数十位
+--状态01：numO2显示设定瓶数个位
+--状态10：numO4显示每瓶十位
+--状态11：numO5显示每瓶个位（同时也是就绪状态）
 --开始装瓶：前2位为当前瓶内已装片数，前3位为从开始以来总共装片个数
---TODO：来瓶脉冲控制/halt、停机时闪烁（LED？)
+--TODO：非法输入判断（瓶数/片数为0）
 ENTITY TabletSys IS
 	PORT (
 		clkI, tabI: IN std_logic;	--时钟、药片脉冲，自动输入--	--时钟脉冲使用CP3(1Hz)--
@@ -40,7 +40,7 @@ ENTITY TabletSys IS
 		--PillsPerBottleL: buffer std_logic_vector(3 downto 0);--每瓶片数，2个BCD--
 		--BottleReady: buffer std_logic := '1';
 		--BottleFull: buffer std_logic :='0';
-		
+		--BottlesLimitL: BUFFER std_logic_vector(3 downto 0);--最大可装瓶数--
 		startPulse: IN std_logic;--开始脉冲--
 
 		haltK, nextK: IN std_logic	--中止开关/换瓶脉冲，手动输入，强制干涉--
@@ -61,6 +61,8 @@ architecture Sys of TabletSys is
 	signal PillsCountL: std_logic_vector(3 downto 0);--总装片数计数器---
 	signal BottlesCountH: std_logic_vector(3 downto 0);--目前已装瓶数--
 	signal BottlesCountL: std_logic_vector(3 downto 0);--目前已装瓶数--
+	signal BottlesLimitH: std_logic_vector(3 downto 0);--最大可装瓶数--
+	signal BottlesLimitL: std_logic_vector(3 downto 0);--最大可装瓶数--
 	signal finished: std_logic :='0';--完成Flag--
 	signal BottleReady: std_logic := '1';
 	signal BottleFull: std_logic :='0';
@@ -71,10 +73,10 @@ begin
 	begin 
 	 if(modeK'event and modeK='1') then --切换输入状态
 	  case status is 
-		when "00"=>status<="01"; --等待有效输入--
-		when "01"=>status<="10"; --每瓶十位--
-		when "10"=>status<="11"; --每瓶个位--
-		when "11"=>status<="00"; --准备就绪--
+		when "00"=>status<="01"; --每瓶十位--
+		when "01"=>status<="10"; --每瓶个位--
+		when "10"=>status<="11"; --瓶数个位--
+		when "11"=>status<="00"; --瓶数个位+就绪--
 	  end case;
 	 end if;
 	end process;
@@ -83,12 +85,11 @@ begin
 
 	process(setK,modeK,status)--输入数值--
 	begin
-		--case modeK is
-		--	when '1'=>
-		--		if(status = "10" or status = "01") then--防止被切换到就绪状态时清零
-		--		num<="0000";--切换模式时置0（异步）--
-		--		end if;
-		--	when '0'=>
+		case modeK is
+			when '1'=>
+					num<="0000";--切换模式时置0（异步）--
+				--end if;
+			when '0'=>
 				if(setK'event and setK='1') then 
 		   			if(num="1001") then --超过9时回0--
 					num<="0000";
@@ -96,21 +97,29 @@ begin
 			 		num<=num+1;
 					end if;
 				end if;
-		--	when others=>NULL;
-	--	end case;
+			when others=>NULL;
+		end case;
 	end process;
 
 
-	process(num)--将暂存数放入每瓶片数寄存器--
+	process(setK)--将暂存数放入每瓶片数寄存器--
 	begin
 		case status is
-			when "01"=>--输入十位状态--
-				if(num <= "0100") then--十位为5以上时不接受，最大片数49----不加底下兜底时，为甚么0101会被赋进去？？
+			when "00"=>
+				if(num <= "0001") then--十位为1以上时不接受，最大瓶数19----
+					BottlesLimitH<=num;--暂存数打入每瓶片数寄存器高位--
+				else 
+					BottlesLimitH<="0001";--兜下来，不允许增加
+				end if;
+			when "01"=>BottlesLimitL<=num;
+			when "10"=>--输入每瓶十位状态--
+				if(num <= "0100") then--十位为5以上时不接受，最大片数49----不加底下兜底时，0101会被赋进去
 					PillsPerBottleH<=num;--暂存数打入每瓶片数寄存器高位--
 				else 
 					PillsPerBottleH<="0100";--兜下来，不允许增加
 				end if;
-			when "10"=>PillsPerBottleL<=num;--暂存数打入每瓶片数寄存器低位--
+			when "11"=>PillsPerBottleL<=num;--暂存数打入每瓶片数寄存器低位--
+			
 			when others=>NULL;--其他情况不改变
 	end case;
 	end process;
@@ -146,7 +155,7 @@ begin
 	begin
 		redLED<='0';
 		greenLED<='0';	
-		if(finished='1' or (num>"0100" and status="01") or (haltK = VCC)) then
+		if(finished='1' or (num>"0001" and status="00") or (num>"0100" and status="10") or (haltK = VCC)) then
 			redLED<='1';
 		end if;
 		if(started='1' and status="11" and finished='0' ) then
@@ -176,6 +185,14 @@ begin
 		aeb => BottleFull
 	);
 	
+	FinishedJudge: comparator8 PORT MAP(
+		dataa(7 downto 4) => BottlesCountH,
+		dataa(3 downto 0) => BottlesCountL,
+		datab(7 downto 4) => BottlesLimitH,
+		datab(3 downto 0) => BottlesLimitL,
+		aeb => finished
+	);
+	
 	PillsCounter: counterDC PORT MAP(
 		clkI => tabI and started and not(finished or nextK or haltK),
 		clrKn => started,
@@ -184,50 +201,41 @@ begin
 		qO(3 downto 0) => PillsCountL
 	);
 	
-	botO <= BottleFull or nextK;
+	botO <= started and (BottleFull or nextK);
 
 	--装片过程处理--
-	process(tabI)
-	variable BottlesCount: integer range 0 to 19 :=0 ;--瓶数计数器，达到上限停止，上限19瓶--
-	begin
-		if(haltK = GND) then--急停管理
-			if(BottlesCount = 19 ) then --总瓶数到达停止--
-				finished<='1';
-			else --装瓶进行--
-				finished<='0';
-				if(tabI'event and tabI = '1') then --开始装瓶--
-					if(started='1' and status="11") then
-					--装片进行中--
-						if (BottleFull = VCC) then
-							BottlesCount:=BottlesCount+1;--已装瓶计数+1
-						end if;
-					end if;
-				end if;
-			end if;
-		end if;
-		
-	end process;
+	
 	
 	--异步输出--
-	Process(status,PillsPerBottleH,BottlesCountL,PillsInBottleL,PillsCountL)
+	Process(status,PillsPerBottleH,BottlesCountL,PillsInBottleL,PillsCountL,started)
 	begin
 		case status is
-			--when "00"=>numO<="11111111111111111111"; --等待有效输入--
-			when "01"=>numO1<=PillsPerBottleH; --输入每瓶片数十位，超出时亮红灯并按最大十位存入，但此时仍可见--
-			when "10"=>numO2<=PillsPerBottleL; --输入每瓶片数个位--
+			when "00"=>--输入瓶数十位--
+				numO1<=BottlesLimitH;
+				numO2<="1111";
+				numO3<="1111";
+				numO4<="1111";
+				numO5<="1111";
+			when "01"=> --输入瓶数个位--
+				numO2<=BottlesLimitL;
+			when "10"=>numO4<=PillsPerBottleH; --输入每瓶片数十位--
 			when "11"=>--前2个是已装每瓶片数计数，后3个是总装片数计数--
-				if(Displaytoggle='0') then
-					numO1<=BottlesCountH;
-					numO2<=BottlesCountL;
-					numO3<="1111";
-					numO4<=PillsInBottleH;
-					numO5<=PillsInBottleL;
+				if(started='0') then
+					numO5<=PillsPerBottleL;
 				else
-					numO1<="1111";
-					numO2<="1111";
-					numO3<=PillsCountH;
-					numO4<=PillsCountM;
-					numO5<=PillsCountL;
+					if(Displaytoggle='0') then
+						numO1<=BottlesCountH;
+						numO2<=BottlesCountL;
+						numO3<="1111";
+						numO4<=PillsInBottleH;
+						numO5<=PillsInBottleL;
+					else
+						numO1<="1111";
+						numO2<="1111";
+						numO3<=PillsCountH;
+						numO4<=PillsCountM;
+						numO5<=PillsCountL;
+					end if;
 				end if;
 			when others=>null;
 		end case;
