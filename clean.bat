1fbc0839d08947803763224d72e267128855867b
why2001x <dnd2001@qq.com>
2020-07-15 10:11:04 +0800

    clean.bat backup

---
+++ clean.bat
@@ -0,0 +1,11 @@
+@echo on
+echo Delete QuartusIIkk intermediate files
+echo Begin deleting...
+if exist ".\db" rd /S /Q ".\db"
+if exist ".\incremental_db" rd /S /Q ".\incremental_db"
+if exist ".\output_files" rd /S /Q ".\output_files"
+if exist ".\simulation" rd /S /Q ".\simulation"
+if exist ".\greybox_tmp" rd /S /Q ".\greybox_tmp"
+for /r . %%c in (*.bak *.temp *.qdf *.qws *.qip *.cmp) do del "%%c"
+echo End deleting...
+pause
